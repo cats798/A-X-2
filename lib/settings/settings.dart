cc322e31abfd94df20aefd33b608e5c44c29abc4
卡茨特 <liyuanlin959@gmail.com>
2026-03-21 14:26:56 +0800

    Add files via upload

---
+++ lib/settings/settings.dart
@@ -0,0 +1,170 @@
+import 'package:anymex/screens/other_features.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_about.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_accounts.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_backup.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_common.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_experimental.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_extensions.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_player.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_reader.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_storage_manager.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_theme.dart';
+import 'package:anymex/screens/settings/sub_settings/settings_ui.dart';
+import 'package:anymex/utils/function.dart';
+import 'package:anymex/utils/logger.dart';
+import 'package:anymex/utils/theme_extensions.dart';
+import 'package:anymex/widgets/common/custom_tiles.dart';
+import 'package:anymex/widgets/common/glow.dart';
+import 'package:anymex/widgets/helper/platform_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:iconly/iconly.dart';
+import 'package:super_sliver_list/super_sliver_list.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Glow(
+        child: Scaffold(
+            body: Column(children: [
+      const NestedHeader(title: 'Settings'),
+      Expanded(
+          child: SuperListView(
+        padding: getResponsiveValue(context,
+            mobileValue: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
+            desktopValue: const EdgeInsets.fromLTRB(20.0, 20.0, 25.0, 20.0)),
+        children: [
+          const SizedBox(height: 30),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color:
+                    Theme.of(context).colorScheme.surfaceContainer.opaque(0.3)),
+            child: Column(
+              children: [
+                CustomTile(
+                    icon: IconlyLight.profile,
+                    title: "Accounts",
+                    description:
+                        "Manage your MyAnimeList, Anilist, Simkl Accounts!",
+                    onTap: () {
+                      navigate(() => const SettingsAccounts());
+                    }),
+                CustomTile(
+                    icon: HugeIcons.strokeRoundedBulb,
+                    title: "Common",
+                    description: "Tweak Settings",
+                    onTap: () {
+                      navigate(() => const SettingsCommon());
+                    }),
+                CustomTile(
+                    icon: HugeIcons.strokeRoundedLibraries,
+                    title: "Backup & Restore",
+                    description: "Backup and restore your library",
+                    onTap: () {
+                      navigate(() => const BackupRestorePage());
+                    }),
+                CustomTile(
+                    icon: Icons.storage_rounded,
+                    title: "Storage Manager",
+                    description:
+                        "Manage cached images, thresholds, and reset app data",
+                    onTap: () {
+                      navigate(() => const SettingsStorageManager());
+                    }),
+                CustomTile(
+                    icon: HugeIcons.strokeRoundedPaintBoard,
+                    title: "UI",
+                    description: "Customize the interface to your liking",
+                    onTap: () {
+                      navigate(() => const SettingsUi());
+                    }),
+                CustomTile(
+                    icon: HugeIcons.strokeRoundedPlay,
+                    title: "Player",
+                    description: "Play around with the video player",
+                    onTap: () {
+                      navigate(() => const SettingsPlayer());
+                    }),
+                CustomTile(
+                    icon: Icons.menu_book_rounded,
+                    title: "Reader",
+                    description: "Configure manga and novel reader defaults",
+                    onTap: () {
+                      navigate(() => const SettingsReader());
+                    }),
+                CustomTile(
+                    icon: HugeIcons.strokeRoundedPaintBrush01,
+                    title: "Theme",
+                    description: "Personalize the look and make it yours",
+                    onTap: () {
+                      navigate(() => const SettingsTheme());
+                    }),
+                const SizedBox(height: 10),
+                CustomTile(
+                    icon: Icons.extension_rounded,
+                    title: "Extensions",
+                    description: "Extensions tailored to your needs",
+                    onTap: () {
+                      navigate(() => const SettingsExtensions());
+                    }),
+                const SizedBox(height: 10),
+                CustomTile(
+                  icon: HugeIcons.strokeRoundedInformationCircle,
+                  title: "Experimental",
+                  description:
+                      "Experimental Settings that are still being tested",
+                  onTap: () async {
+                    navigate(() => const SettingsExperimental());
+                  },
+                ),
+                const SizedBox(height: 10),
+                CustomTile(
+                  icon: HugeIcons.strokeRoundedFile01,
+                  title: "Share Logs",
+                  description: "Share Logs of the App",
+                  onTap: () async => await Logger.share(),
+                ),
+                const SizedBox(height: 10),
+                CustomTile(
+                  icon: HugeIcons.strokeRoundedInformationCircle,
+                  title: "About",
+                  description: "About the App",
+                  onTap: () async {
+                    navigate(() => const AboutPage());
+                  },
+                ),
+              ],
+            ),
+          ),
+          30.height(),
+        ],
+      )),
+    ])));
+  }
+}
+
+class CustomBackButton extends StatelessWidget {
+  const CustomBackButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor:
+                Theme.of(context).colorScheme.surfaceContainer.opaque(0.5)),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: const Icon(Icons.arrow_back_ios_new_rounded));
+  }
+}
---
+++ lib/settings/sub_settings/settings_common.dart
@@ -0,0 +1,173 @@
+import 'package:anymex/controllers/service_handler/service_handler.dart';
+import 'package:anymex/controllers/settings/settings.dart';
+import 'package:anymex/database/data_keys/keys.dart';
+import 'package:anymex/widgets/common/custom_tiles.dart';
+import 'package:anymex/widgets/common/glow.dart';
+import 'package:anymex/widgets/custom_widgets/custom_expansion_tile.dart';
+import 'package:anymex/widgets/helper/platform_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:anymex/utils/theme_extensions.dart';
+import 'package:get/get.dart';
+import 'package:super_sliver_list/super_sliver_list.dart';
+import 'package:anymex/screens/other_features.dart';
+
+class SettingsCommon extends StatefulWidget {
+  const SettingsCommon({super.key});
+
+  @override
+  State<SettingsCommon> createState() => _SettingsCommonState();
+}
+
+class _SettingsCommonState extends State<SettingsCommon> {
+  final settings = Get.find<Settings>();
+  late bool uniScrapper;
+  late bool shouldAskForPermission = General.shouldAskForTrack.get<bool>(true);
+  late bool hideAdultContent = General.hideAdultContent.get<bool>(true);
+  bool get isMal => serviceHandler.serviceType.value.isMal;
+  late Map<String, bool> homePageCards;
+
+  @override
+  void initState() {
+    super.initState();
+    uniScrapper = General.universalScrapper.get<bool>(false);
+    homePageCards = isMal ? settings.homePageCardsMal : settings.homePageCards;
+    homePageCards.putIfAbsent('Recommended Animes', () => true);
+    homePageCards.putIfAbsent('Recommended Mangas', () => true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Glow(
+      child: Scaffold(
+        body: Column(
+          children: [
+            const NestedHeader(title: 'Common'),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: getResponsiveValue(context,
+                      mobileValue:
+                          const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
+                      desktopValue:
+                          const EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 20.0)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AnymexExpansionTile(
+                        initialExpanded: true,
+                        title: 'Universal',
+                        content: Column(
+                          children: [
+                            CustomSwitchTile(
+                                icon: Icons.touch_app_rounded,
+                                title: 'Ask for tracking permission',
+                                description:
+                                    'If enabled, Anymex will ask for tracking permission if not then it will track by default.',
+                                switchValue: shouldAskForPermission,
+                                onChanged: (e) {
+                                  setState(() {
+                                    shouldAskForPermission = e;
+                                    General.shouldAskForTrack.set(e);
+                                  });
+                                }),
+                            CustomSwitchTile(
+                                icon: Icons.play_disabled_rounded,
+                                title: 'Hide Adult Content',
+                                description:
+                                    'If enabled, you will not get a prompt for enabling adult content on Anilist/MyAnimeList.',
+                                switchValue: hideAdultContent,
+                                onChanged: (e) {
+                                  setState(() {
+                                    hideAdultContent = e;
+                                    General.hideAdultContent.set(e);
+                                  });
+                                }),
+                            Obx(
+                              () => CustomSwitchTile(
+                                icon: Icons.play_circle_fill_rounded,
+                                title: 'Show Continue Watching Card',
+                                description:
+                                    'Display Continue Watching cards on home page from offline progress.',
+                                switchValue: settings.showContinueWatchingCard,
+                                onChanged: (e) =>
+                                    settings.showContinueWatchingCard = e,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      AnymexExpansionTile(
+                          initialExpanded: true,
+                          title: 'Anilist',
+                          content: CustomTile(
+                            icon: Icons.format_list_bulleted_sharp,
+                            title: 'Manage Anilist Lists',
+                            description:
+                                "Choose which list to show on home page",
+                            onTap: () => _showHomePageCardsDialog(),
+                          )),
+                      AnymexExpansionTile(
+                          initialExpanded: true,
+                          title: 'MyAnimeList',
+                          content: CustomTile(
+                            icon: Icons.format_list_bulleted_sharp,
+                            title: 'Manage MyAnimeList Lists',
+                            description:
+                                "Choose which list to show on home page",
+                            onTap: () => _showHomePageCardsDialog(),
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showHomePageCardsDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Manage Home Page Cards"),
+          content: SizedBox(
+            width: double.maxFinite,
+            child: Obx(() {
+              final homePageCards =
+                  isMal ? settings.homePageCardsMal : settings.homePageCards;
+              return SuperListView.builder(
+                shrinkWrap: true,
+                itemCount: homePageCards.length,
+                itemBuilder: (context, index) {
+                  final key = homePageCards.keys.elementAt(index);
+                  final value = homePageCards[key]!;
+
+                  return CheckboxListTile(
+                    title: Text(key),
+                    value: value,
+                    onChanged: (bool? newValue) {
+                      if (newValue != null) {
+                        isMal
+                            ? settings.updateHomePageCardMal(key, newValue)
+                            : settings.updateHomePageCard(key, newValue);
+                      }
+                    },
+                  );
+                },
+              );
+            }),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
