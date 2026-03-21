943a3383ebe8517d1ccdf097d68bc57a39f7b33f
卡茨特 <liyuanlin959@gmail.com>
2026-03-22 00:08:24 +0800

    更新 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart'; 
+import 'package:anymex/l10n/app_localizations.dart'; 
 import 'package:anymex/generated/app_localizations.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
