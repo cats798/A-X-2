d01a41a4b703f39d4cfebee884041cc43c78b9aa
卡茨特 <liyuanlin959@gmail.com>
2026-03-21 14:14:02 +0800

    Add files via upload

---
+++ lib/controllers/services/widgets/widgets_builders.dart
@@ -0,0 +1,108 @@
+import 'package:anymex/models/Media/media.dart';
+import 'package:anymex/utils/function.dart';
+import 'package:anymex/widgets/common/big_carousel.dart';
+import 'package:anymex/widgets/common/future_reusable_carousel.dart';
+import 'package:anymex/widgets/common/reusable_carousel.dart';
+import 'package:anymex/widgets/custom_widgets/custom_text.dart';
+import 'package:anymex_extension_runtime_bridge/Models/Source.dart';
+import 'package:anymex_extension_runtime_bridge/anymex_extension_runtime_bridge.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Widget buildSection(String title, dynamic data,
+    {DataVariant variant = DataVariant.regular,
+    bool isLoading = false,
+    ItemType type = ItemType.anime,
+    Source? source}) {
+  if (data is Stream) {
+    return StreamBuilder(
+      stream: data,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return buildLoader(title);
+        }
+        return ReusableCarousel(
+          data: snapshot.data ?? [],
+          title: title,
+          type: type,
+          variant: variant,
+          isLoading: isLoading,
+          source: source,
+        );
+      },
+    );
+  }
+  return ReusableCarousel(
+    data: data,
+    title: title,
+    type: type,
+    variant: variant,
+    isLoading: isLoading,
+    source: source,
+  );
+}
+
+Widget buildLoader(String title) {
+  return ReusableCarousel(
+    data: const [],
+    title: title,
+    isLoading: true,
+  );
+}
+
+Container buildChip(String label) {
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+    margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+    decoration: BoxDecoration(
+      color: Get.theme.colorScheme.primary,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: FittedBox(
+      fit: BoxFit.scaleDown,
+      child: AnymexText(
+        text: label,
+        variant: TextVariant.bold,
+        color: Get.theme.colorScheme.onPrimary,
+        size: 14,
+      ),
+    ),
+  );
+}
+
+Widget buildBigCarousel(List<Media> data, bool isManga, {CarouselType? type}) {
+  return BigCarousel(
+      data: data,
+      carouselType:
+          type ?? (isManga ? CarouselType.manga : CarouselType.anime));
+}
+
+Widget buildMangaSection(String title, List<Media> data,
+    {bool isAnilist = false}) {
+  return ReusableCarousel(
+    data: data,
+    title: title,
+    type: ItemType.manga,
+    variant: isAnilist ? DataVariant.anilist : DataVariant.regular,
+  );
+}
+
+Widget buildFutureSection(
+  String title,
+  Future<List<dynamic>> future, {
+  DataVariant variant = DataVariant.regular,
+  ItemType type = ItemType.anime,
+  Source? source,
+  Widget? errorWidget,
+  Widget? emptyWidget,
+}) {
+  return FutureReusableCarousel(
+    future: future,
+    title: title,
+    variant: variant,
+    type: type,
+    source: source,
+    errorWidget: errorWidget,
+    emptyWidget: emptyWidget,
+  );
+}
---
+++ lib/database/data_keys/keys.dart
@@ -0,0 +1,244 @@
+import 'package:anymex/database/kv_helper.dart';
+export 'package:anymex/database/kv_helper.dart';
+
+enum General {
+  shouldAskForTrack,
+  hideAdultContent,
+  uiScaler,
+  isFirstTime,
+  hasAcceptedCommentRules,
+  universalScrapper,
+  enableBetaUpdates,
+  imageCacheThresholdGb,
+  libraryGridAutoMigrated,
+}
+
+enum ThemeKeys {
+  isLightMode,
+  isSystemMode,
+  isOled,
+  selectedVariantIndex,
+  themeMode,
+  customColorIndex,
+  logoAnimationType,
+}
+
+enum PlayerKeys { useLibass, useMediaKit }
+
+enum PlayerUiKeys {
+  playerExperimentalEnabled,
+  bottomControlsSettings,
+  playerControlTheme,
+  playerControlThemesJson,
+  mediaIndicatorTheme,
+  mpvCoreSettings,
+  betterPlayerCoreSettings,
+  mpvVisualSettings,
+  currentVisualProfile,
+  currentVisualSettings,
+  selectedShader,
+  selectedShaderLegacy,
+  selectedProfile,
+  shadersEnabled,
+  cacheDays,
+}
+
+enum ReaderKeys {
+  readerControlTheme,
+  readingLayout,
+  readingDirection,
+  imageWidth,
+  scrollSpeed,
+  spacedPages,
+  overscrollToChapter,
+  preloadPages,
+  showPageIndicator,
+  cropImages,
+  volumeKeysEnabled,
+  invertVolumeKeys,
+  dualPageMode,
+  autoScrollEnabled,
+  autoScrollSpeed,
+  customBrightnessEnabled,
+  customBrightnessValue,
+  colorFilterEnabled,
+  colorFilterValue,
+  colorFilterMode,
+  grayscaleEnabled,
+  invertColorsEnabled,
+  readerTheme,
+  keepScreenOn,
+  alwaysShowChapterTransition,
+  longPressPageActionsEnabled,
+  autoWebtoonMode,
+  displayRefreshEnabled,
+  displayRefreshDurationMs,
+  displayRefreshInterval,
+  displayRefreshColor,
+}
+
+enum NovelReaderKeys {
+  themeMode,
+  backgroundOpacity,
+  fontSize,
+  lineHeight,
+  letterSpacing,
+  wordSpacing,
+  paragraphSpacing,
+  fontFamily,
+  textAlign,
+  paddingHorizontal,
+  paddingVertical,
+  autoScroll,
+  autoScrollSpeed,
+  volumeScrolling,
+  tapToScroll,
+  keepScreenOn,
+  verticalSeekbar,
+  swipeGestures,
+  pageReader,
+  showReadingProgress,
+  showBatteryTime,
+  ttsSpeed,
+  ttsPitch,
+  ttsVoice,
+  ttsAutoAdvance,
+  ttsEnabled,
+  overscrollToChapter,
+}
+
+enum LocalSourceKeys {
+  watchOfflinePath,
+  watchOfflinePathHistory,
+  watchOfflineDownloadPath,
+  watchOfflineDownloadPathHistory,
+}
+
+enum ServiceKeys { serviceType }
+
+enum SyncKeys {
+  gistGithubToken,
+  gistGithubUsername,
+  gistAutoDeleteCompleted,
+  gistExitSyncNotifications,
+}
+
+enum SourceKeys {
+  activeAnimeRepo,
+  activeMangaRepo,
+  activeNovelRepo,
+  activeAniyomiAnimeRepo,
+  activeAniyomiMangaRepo,
+  extensionsServiceAllowed,
+  activeSourceId,
+  activeMangaSourceId,
+  activeNovelSourceId,
+  animeExtensionOrder,
+  mangaExtensionOrder,
+  novelExtensionOrder,
+}
+
+enum AuthKeys {
+  authToken,
+  malAuthToken,
+  malRefreshToken,
+  simklAuthToken,
+  malSessionId,
+}
+
+enum SearchKeys { novelSearchedQueries }
+
+enum LibraryKeys { libraryLastType }
+
+enum TapZoneKeys {
+  tapZonesPaged,
+  tapZonesPagedVertical,
+  tapZonesWebtoon,
+  tapZonesWebtoonHorizontal,
+  tapZonesEnabled,
+}
+
+enum DynamicKeys {
+  trackingPermission,
+  watchProgress,
+  customSetting,
+  searchHistory,
+  libraryLastListIndex,
+  librarySortType,
+  librarySortOrder,
+  libraryGridSize,
+  mappedMediaTitle,
+  offlineVideoProgress;
+
+  T get<T>(dynamic id, [T? defaultValue]) {
+    return KvHelper.get<T>('${name}_$id', defaultVal: defaultValue);
+  }
+
+  void set<T>(dynamic id, T value) {
+    KvHelper.set('${name}_$id', value);
+  }
+
+  void delete(dynamic id) {
+    KvHelper.remove('${name}_$id');
+  }
+}
+
+enum PlayerSettingsKeys {
+  speed,
+  resizeMode,
+  showSubtitle,
+  subtitleSize,
+  subtitleColor,
+  subtitleFont,
+  subtitleBackgroundColor,
+  subtitleOutlineColor,
+  skipDuration,
+  seekDuration,
+  bottomMargin,
+  transculentControls,
+  defaultPortraitMode,
+  playerStyle,
+  subtitleOutlineWidth,
+  autoSkipOP,
+  autoSkipED,
+  autoSkipOnce,
+  autoSkipRecap,
+  enableSwipeControls,
+  markAsCompleted,
+  transitionSubtitle,
+  autoTranslate,
+  translateTo,
+  autoSkipFiller,
+  enableScreenshot,
+  subtitleOpacity,
+  subtitleBottomMargin,
+  subtitleOutlineType,
+}
+
+enum UISettingsKeys {
+  glowMultiplier,
+  radiusMultiplier,
+  saikouLayout,
+  tabBarHeight,
+  tabBarWidth,
+  tabBarRoundness,
+  compactCards,
+  cardRoundness,
+  blurMultipler,
+  animationDuration,
+  translucentTabBar,
+  glowDensity,
+  homePageCards,
+  enableAnimation,
+  disableGradient,
+  homePageCardsMal,
+  cardStyle,
+  historyCardStyle,
+  liquidMode,
+  liquidBackgroundPath,
+  retainOriginalColor,
+  usePosterColor,
+  enablePosterKenBurns,
+  carouselStyle,
+  showContinueWatchingCard,
+}
