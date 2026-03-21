06240cd9ccfca56f76a131d36f37e2a54e0a29ee
卡茨特 <liyuanlin959@gmail.com>
2026-03-21 23:43:22 +0800

    更新 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart'; 
 import 'package:anymex/generated/app_localizations.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
