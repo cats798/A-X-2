5dd5f01162be7363c50a373292cd71916d3ba03e
卡茨特 <liyuanlin959@gmail.com>
2026-04-13 17:28:56 +0800

    更新 app_localizations.dart

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -1318,6 +1318,7 @@ class AppLocalizations {
   String get specialThanks => _strings['specialThanks']!;
   String get communityContributors => _strings['communityContributors']!;
   String get staff => _strings['staff']!;
+  String get episode => _strings['episode']!;
   String get noContributors => _strings['noContributors']!;
   String get failedToLoadContributors => _strings['failedToLoadContributors']!;
   String get cardStyle => _strings['cardStyle']!;
